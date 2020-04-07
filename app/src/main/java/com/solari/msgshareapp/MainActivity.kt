221107483533bff897306e8e7e52a1710c3fad43
Solariii <eslamsolari1996@gmail.com>
2020-04-08 00:50:14 +0200

    Initial Commit

---
+++ app/src/main/java/com/solari/msgshareapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.solari.msgshareapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnShowToast.setOnClickListener {
+            Toast.makeText(this, "Button was clicked !", Toast.LENGTH_LONG).show()
+        }
+        btnSendMsgToNextActivity.setOnClickListener {
+
+            val message : String = etUserMessage.text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("user_message", message)
+            startActivity(intent)
+        }
+
+        btnShareToOtherApps.setOnClickListener {
+            val message : String = etUserMessage.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Share to : "))
+        }
+    }
+}
